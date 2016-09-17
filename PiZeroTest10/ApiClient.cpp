b50e64d6100aacf5091f95cae60d420b0336880e
Rob Pinson <pinson_rob@yahoo.co.uk>
2016-09-17 21:54:39 +0100

    ReadMe added

--- PiZeroTest10/ApiClient.cpp
+++ PiZeroTest10/ApiClient.cpp
@@ -7,50 +7,50 @@
 #include <QUrlQuery>
 
 Q_DECLARE_METATYPE(Dtos::PeopleResponse)
-Q_DECLARE_METATYPE(Dtos::Person)	
-	
+Q_DECLARE_METATYPE(Dtos::Person)
+
 ApiClient::ApiClient()
 {
-	baseUrl = QString("http://192.168.0.50:8089/api/");
-	//baseUrl = QString("http://trackerdemosite.azurewebsites.net/api/");
+	//baseUrl = QString("http://office-pc:55577/api/");
+	baseUrl = QString("http://192.168.0.50:55577/api/");
 }
 
 void ApiClient::GetPeople(const QList<int>& excludeIds, const int& deviceId)
 {
 	QJsonObject json;
 	QString exIds;
-	
+
 	for (int i = 0; i < excludeIds.size(); i++)
-	{		
+	{
 		exIds += QString::number(excludeIds[i]);
 		if (i < excludeIds.size() - 1)
 			exIds += ",";
 	}
-	
+
 	json.insert("ExcludeIds", exIds);
-	json.insert("DeviceId", deviceId);	
+	json.insert("DeviceId", deviceId);
 
 	QNetworkAccessManager* mgr = new QNetworkAccessManager(this);
-		
-	connect(mgr, 
-		SIGNAL(finished(QNetworkReply*)), 
-		this, 
-		SLOT(onGetPeopleResponse(QNetworkReply*)));	
-	
+
+	connect(mgr,
+		SIGNAL(finished(QNetworkReply*)),
+		this,
+		SLOT(onGetPeopleResponse(QNetworkReply*)));
+
 	QUrl url(baseUrl + "People");
-	QUrlQuery query;	
+	QUrlQuery query;
 	query.addQueryItem("deviceId", QString::number(deviceId));
 	query.addQueryItem("excludeids", exIds);
-	
-	url.setQuery(query);	
-	
-	QNetworkRequest request(url);	
+
+	url.setQuery(query);
+
+	QNetworkRequest request(url);
 	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-	
+
 	qDebug() << "Http GET People";
 	QByteArray data = QJsonDocument(json).toJson();
 	QNetworkReply* reply = mgr->get(request);
-	
+
 	connect(reply,
 		SIGNAL(error(QNetworkReply::NetworkError)),
 		this,
@@ -59,80 +59,80 @@ void ApiClient::GetPeople(const QList<int>& excludeIds, const int& deviceId)
 
 void ApiClient::onGetPeopleResponse(QNetworkReply* reply)
 {
-	qDebug() << "Http GET People response";	
-	
+	qDebug() << "Http GET People response";
+
 	if (reply->error() != QNetworkReply::NoError)
 	{
-		QString message;		
-		message = QString("Error calling API GET People: ") + QString::number(reply->error());		
-		emit requestError(message);	
+		QString message;
+		message = QString("Error calling API GET People: ") + QString::number(reply->error());
+		emit requestError(message);
 	}
 	else
 	{
 		//Get data
 		Dtos::PeopleResponse peopleResponse;
-		
+
 		QByteArray responseData = reply->readAll();
-		
+
 		if (responseData.isEmpty())
 		{
 			qDebug() << "Get People returned no data.";
-			
+
 			peopleResponse.IsSuccess = false;
 			peopleResponse.ErrorMessage = "No Data Returned";
-			
+
 			emit getPeopleResponse(peopleResponse);
-			
+
 			return;
 		}
-		
+
 		QJsonParseError error;
 		QJsonDocument jsonDoc = QJsonDocument::fromJson(responseData, &error);
-		
+
 		if (error.error != QJsonParseError::NoError)
 		{
 			qDebug() << "Get People Json Parse error.";
-			
+
 			peopleResponse.IsSuccess = false;
 			peopleResponse.ErrorMessage = "Json Parse Error";
-			
+
 			emit getPeopleResponse(peopleResponse);
-			
+
 			return;
 		}
-		
+
 		if ((jsonDoc.isNull() || jsonDoc.isEmpty()))
 		{
 			qDebug() << "Get People Json is empty.";
-			
+
 			peopleResponse.IsSuccess = false;
 			peopleResponse.ErrorMessage = "Json is empty";
-			
+
 			emit getPeopleResponse(peopleResponse);
-			
+
 			return;
 		}
-		
-		QJsonObject jsonObject = jsonDoc.object();			
-		QJsonArray jsonArray = jsonObject["People"].toArray();
-		
+
+		QJsonObject jsonObject = jsonDoc.object();
+		QJsonArray jsonArray = jsonObject["people"].toArray();
+
 		foreach(const QJsonValue & val, jsonArray)
 		{
 			QJsonObject obj = val.toObject();
 			Dtos::Person person;
-			person.Id = obj["Id"].toInt();
-			person.Name = obj["Name"].toString();
-			person.Image = QByteArray::fromBase64(obj["Image"].toString().toLatin1());	
-			person.CardUid = obj["CardUid"].toString();
-			person.InLocation = obj["InLocation"].toBool();
-			
+			person.Id = obj["id"].toInt();
+			person.Name = obj["name"].toString();
+			person.Image = QByteArray::fromBase64(obj["image"].toString().toLatin1());
+			person.CardUid = obj["cardUid"].toString();
+			person.InLocation = obj["inLocation"].toBool();
+
 			peopleResponse.people.append(person);
-		}		
-				
+		}
+
 		qDebug() << "GET People returned: " + QString::number(peopleResponse.people.count());
-		
+
 		peopleResponse.IsSuccess = true;
-		emit getPeopleResponse(peopleResponse);	
+		emit getPeopleResponse(peopleResponse);
 	}
 	reply->abort();
 	reply->deleteLater();
@@ -147,17 +147,17 @@ void ApiClient::PostMovement(const QString& cardId, const int& deviceId, const Q
 	json.insert("SwipeTime", swipeTime);
 
 	QNetworkAccessManager* mgr = new QNetworkAccessManager(this);
-		
-	connect(mgr, SIGNAL(finished(QNetworkReply*)), this, SLOT(onResult(QNetworkReply*)));	
-	
+
+	connect(mgr, SIGNAL(finished(QNetworkReply*)), this, SLOT(onResult(QNetworkReply*)));
+
 	QUrl url(baseUrl + "Movement");
-	QNetworkRequest request(url);	
+	QNetworkRequest request(url);
 	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-	
+
 	qDebug() << "Http Post JSON";
 	QByteArray data = QJsonDocument(json).toJson();
 	QNetworkReply* reply = mgr->post(request, data);
-	
+
 	connect(reply,
 		SIGNAL(error(QNetworkReply::NetworkError)),
 		this,
@@ -170,27 +170,27 @@ void ApiClient::onResult(QNetworkReply* reply)
 
 	if (reply->error() != QNetworkReply::NoError)
 	{
-		QString message;		
-		message = QString("Error calling API: ") + QString::number(reply->error());		
-		emit requestError(message);	
+		QString message;
+		message = QString("Error calling API: ") + QString::number(reply->error());
+		emit requestError(message);
 	}
 	else
 	{
 		Dtos::Person person;
-	
+
 		QByteArray response_data = reply->readAll();
 		QJsonDocument json = QJsonDocument::fromJson(response_data);
 		QJsonObject jsonObject = json.object();
-	
-		person.Id = jsonObject["Id"].toInt();
-		person.CardUid = jsonObject["CardUid"].toString();
-		person.InLocation = jsonObject.value("Ingress").toBool();
-		person.Name = jsonObject.value("Name").toString();
-		person.Image = QByteArray::fromBase64(jsonObject.value("Image").toString().toLatin1());	
 
-		emit movementResponse(person);	
-	}	
-		
+		person.Id = jsonObject["id"].toInt();
+		person.CardUid = jsonObject["cardUid"].toString();
+		person.InLocation = jsonObject.value("ingress").toBool();
+		person.Name = jsonObject.value("name").toString();
+		person.Image = QByteArray::fromBase64(jsonObject.value("image").toString().toLatin1());
+
+		emit movementResponse(person);
+	}
+
 	reply->abort();
 	reply->deleteLater();
 	reply->manager()->deleteLater();
@@ -200,9 +200,9 @@ void ApiClient::onError(QNetworkReply::NetworkError err)
 {
 	if (err != QNetworkReply::NoError)
 	{
-		QString message;		
-		message = QString("Error calling API: ") + QString::number(err);		
-		emit requestError(message);	
+		QString message;
+		message = QString("Error calling API: ") + QString::number(err);
+		emit requestError(message);
 	}
 }
 
--- PiZeroTest10/MainWindow.cpp
+++ PiZeroTest10/MainWindow.cpp
@@ -14,7 +14,7 @@
 
 using namespace utility;
 using namespace std;
-	
+
 QMutex mutex;
 
 #define PINGREEN 17
@@ -23,10 +23,11 @@ QMutex mutex;
 #define PINBEEP 27
 
 const QString server = "m21.cloudmqtt.com";
-const QString uname = "apzvvubw";
-const QString pword = "bqqhHe9qGf1A";
+const int port = 15480;
+const QString uname = "regcjdre";
+const QString pword = "sjOPZR3T1ehQ";
 const QString topic = "location/2/movement";
-const int deviceId = 5;
+const int deviceId = 2;
 const int locationId = 2;
 static const QString dbPath = "tracker.db";
 static void OnMessageCallBack();
@@ -38,75 +39,75 @@ MainWindow::MainWindow(QWidget *parent)
 {
 	QApplication::setOverrideCursor(Qt::BlankCursor);
 	setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
-	ui->setupUi(this);	
+	ui->setupUi(this);
 	QTimer::singleShot(500, this, SLOT(showFullScreen()));
-	
-	mClient->setAutoReconnect(true);	
+
+	mClient->setAutoReconnect(true);
 	connect(mClient, SIGNAL(message(QString, QByteArray)), this, SLOT(message(QString, QByteArray)));
 	doConnect();
 	doSubscribe();
-	
-	MyNfcReader = new NfcReader;	
+
+	MyNfcReader = new NfcReader;
 	MyApiClient = new ApiClient;
-		
-	MyDbManager = new DbManager(dbPath);	
-	
+
+	MyDbManager = new DbManager(dbPath);
+
 	//wire up events
 	connect(MyNfcReader,
 		SIGNAL(cardPresent(QString)),
 		SLOT(OnCardPresent(QString)));
-	
+
 	connect(MyNfcReader,
 		SIGNAL(cardRemoved()),
-		SLOT(OnCardRemoved()));	
-		
+		SLOT(OnCardRemoved()));
+
 	connect(MyApiClient,
 		SIGNAL(movementResponse(Dtos::Person)),
 		this,
 		SLOT(OnMovementResponse(Dtos::Person)));
-	
+
 	connect(MyApiClient,
 		SIGNAL(getPeopleResponse(Dtos::PeopleResponse)),
 		this,
 		SLOT(OnPeopleResponse(Dtos::PeopleResponse)));
-	
+
 	QObject::connect(MyApiClient,
 		SIGNAL(requestError(QString)),
 		this,
-		SLOT(OnRequestError(QString)));	
-	
+		SLOT(OnRequestError(QString)));
+
 	//Load people
 	//Call API
-	qDebug() << "Load People: Calling API...";	
+	qDebug() << "Load People: Calling API...";
 	QList<int> excludeIds;
 	MyApiClient->GetPeople(excludeIds, deviceId);
-	
+
 	//Start Load People timer
 	peopleTimer = new QTimer(this);
 	connect(peopleTimer, SIGNAL(timeout()), this, SLOT(OnGetPeople()));
 	peopleTimer->start(60000);
-	
+
 	//Start movement msg timer
 	movementTimer = new QTimer(this);
 	connect(movementTimer, SIGNAL(timeout()), this, SLOT(OnSendMovements()));
 	movementTimer->start(1000);
-	
+
 	//Start NFC Reader thread
 	if (MyNfcReader->init() > 0)
 	{
 		MyNfcReader->start();
-	}	
-	
+	}
+
 	if (wiringPiSetupGpio() == -1)
 	{
 		qDebug() << "wiringPi setup error";
-	}	
-	
+	}
+
 	pinMode(PINRED, OUTPUT);
 	pinMode(PINGREEN, OUTPUT);
 	pinMode(PINBLUE, OUTPUT);
 	pinMode(PINBEEP, OUTPUT);
-	
+
 	digitalWrite(PINRED, HIGH);
 	digitalWrite(PINGREEN, LOW);
 	digitalWrite(PINBLUE, HIGH);
@@ -114,25 +115,25 @@ MainWindow::MainWindow(QWidget *parent)
 
 //TEST CODE
 void MainWindow::doConnect()
-{  	
+{
 	if (!(uname.isEmpty() || pword.isEmpty()))
 	{
 		mClient->setUsernamePassword(uname, pword);
 	}
-	mClient->doConnect(server, 10891);
+	mClient->doConnect(server, port);
 }
 
 bool MainWindow::doPublish(const QString& payload)
 {
 	bool result=true;
-	
+
 	if (mClient->publish(topic, payload) < 0)
 	{
 		qDebug() << "Publish failed";
 		qDebug() << "Reconnecting to broker";
-		
+
 		result = false;
-		
+
 		if (mClient->doReconnect())
 		{
 			if (mClient->publish(topic, payload) < 0)
@@ -142,16 +143,16 @@ bool MainWindow::doPublish(const QString& payload)
 			}
 			else
 			{
-				result = true;	
+				result = true;
 			}
 		}
 	}
-	
+
 	return result;
 }
 
 void MainWindow::doSubscribe()
-{	
+{
 	if (!mClient->subscribe(topic))
 	{
 		qDebug() << "Subscribe failed";
@@ -160,48 +161,48 @@ void MainWindow::doSubscribe()
 
 void MainWindow::message(const QString& topic, const QByteArray& payload)
 {
-	qDebug() << "Window::message" << topic << payload;	
-	
+	qDebug() << "Window::message" << topic << payload;
+
 	QJsonParseError error;
 	QJsonDocument jsonDoc = QJsonDocument::fromJson(payload, &error);
-		
+
 	if (error.error != QJsonParseError::NoError)
 	{
-		qDebug() << "MQTT message, Movement Json Parse error.";			
+		qDebug() << "MQTT message, Movement Json Parse error.";
 		return;
 	}
-		
+
 	if ((jsonDoc.isNull() || jsonDoc.isEmpty()))
 	{
-		qDebug() << "MQTT message, Movement Json is empty.";	
+		qDebug() << "MQTT message, Movement Json is empty.";
 		return;
 	}
-		
+
 	QJsonObject jsonObject = jsonDoc.object();
 	int movementDeviceId = jsonObject["DeviceId"].toInt();
-	
+
 	if (movementDeviceId == deviceId)
 	{
 		qDebug() << "Message ignored: from this device";
 		return;
 	}
-	
+
 	QString movementCardId = jsonObject["CardId"].toString();
 	int movementInLocation = jsonObject["InLocation"].toInt();
-	
+
 	//Update DB
 	Dtos::Person person = MyDbManager->GetPersonByCardId(movementCardId);
-	
+
 	if (person.Id > 0)
 	{
 		//person is known update location status
 		person.InLocation = movementInLocation;
-		
+
 		MyDbManager->UpsertPerson(person);
 		return;
 	}
-	
-	//TODO Get person Data from server....	
+
+	//TODO Get person Data from server....
 }
 
 void MainWindow::doUnsubscribe()
@@ -215,48 +216,48 @@ void MainWindow::doUnsubscribe()
 //END
 
 void MainWindow::OnCardRemoved()
-{	
+{
 }
 
 void MainWindow::OnCardPresent(QString cardId)
-{	
+{
 	//Call API
-	
-	//Local DB cache first	
+
+	//Local DB cache first
 	Dtos::Person person = MyDbManager->GetPersonByCardId(cardId);
-	
+
 	if (person.Name.isEmpty())
 	{
 		qDebug() << "Card Present Not in Cache: Calling API...";
 		QDateTime local(QDateTime::currentDateTime());
 		QDateTime UTC(local.toUTC());
-		MyApiClient->PostMovement(cardId, deviceId, UTC.toString(Qt::ISODate));		
-		
+		MyApiClient->PostMovement(cardId, deviceId, UTC.toString(Qt::ISODate));
+
 		Beep();
-		
+
 		//BLUE LED
 		digitalWrite(PINGREEN, HIGH);
 		digitalWrite(PINBLUE, LOW);
 	}
 	else
 	{
-		person.InLocation = !person.InLocation;		
-		MyDbManager->UpsertPerson(person);		
-			
+		person.InLocation = !person.InLocation;
+		MyDbManager->UpsertPerson(person);
+
 		Beep();
-	
+
 		//BLUE LED
 		digitalWrite(PINGREEN, HIGH);
 		digitalWrite(PINBLUE, LOW);
-		
+
 		ShowPersonDetailsDialog(person);
-	}	
-	
+	}
+
 	//Add movement to DB
 	QDateTime local(QDateTime::currentDateTime());
 	QDateTime UTC(local.toUTC());
-	MyDbManager->AddMovement(cardId, UTC.toString(Qt::ISODate), person.InLocation);	
-	
+	MyDbManager->AddMovement(cardId, UTC.toString(Qt::ISODate), person.InLocation);
+
 	qDebug() << "movement saved to DB";
 }
 
@@ -276,7 +277,7 @@ void MainWindow::Beep()
 void MainWindow::OnPeopleResponse(Dtos::PeopleResponse response)
 {
 	mutex.lock();
-	
+
 	if (response.IsSuccess)
 	{
 		foreach(const Dtos::Person & person, response.people)
@@ -286,9 +287,9 @@ void MainWindow::OnPeopleResponse(Dtos::PeopleResponse response)
 	}
 	else
 	{
-		qDebug() << "Get People Call Error: " + response.ErrorMessage;		
+		qDebug() << "Get People Call Error: " + response.ErrorMessage;
 	}
-	
+
 	mutex.unlock();
 }
 
@@ -297,21 +298,21 @@ void MainWindow::OnMovementResponse(Dtos::Person person)
 	mutex.lock();
 	qDebug() << "On Movement Post Response";
 	qDebug() << "ID: " + QString::number(person.Id);
-	qDebug() << "Name: " + person.Name; 
-		
+	qDebug() << "Name: " + person.Name;
+
 	MyDbManager->UpsertPerson(person);
-	
+
 	ShowPersonDetailsDialog(person);
-	
+
 	mutex.unlock();
 }
 
 void MainWindow::OnRequestError(QString message)
 {
-	qDebug() << "API ERROR: " + message;	
-	
+	qDebug() << "API ERROR: " + message;
+
 	//TODO show Card not recognised screen
-	
+
 	//TODO LED RED then GREEN
 	digitalWrite(PINGREEN, HIGH);
 	digitalWrite(PINBLUE, HIGH);
@@ -319,8 +320,8 @@ void MainWindow::OnRequestError(QString message)
 	delay(500);
 	digitalWrite(PINRED, HIGH);
 	digitalWrite(PINGREEN, LOW);
-	
-	//TODO buzzer error	
+
+	//TODO buzzer error
 	digitalWrite(PINBEEP, HIGH);
 	delay(200);
 	digitalWrite(PINBEEP, LOW);
@@ -335,30 +336,30 @@ void MainWindow::OnGetPeople()
 void MainWindow::OnSendMovements()
 {
 	QList <Dtos::Movement> movements = MyDbManager->GetMovements();
-	
+
 	foreach(Dtos::Movement movement, movements)
 	{
 		//publish movement
 		QJsonObject json;
 		json.insert("CardId", movement.CardId);
-		json.insert("DeviceId", deviceId);	
+		json.insert("DeviceId", deviceId);
 		json.insert("InLocation", movement.InLocation);
 		json.insert("SwipeTime", movement.SwipeTime);
-	
+
 		QJsonDocument doc(json);
 		QString strJson(doc.toJson());
-	
+
 		//send message
 		if (doPublish(strJson))
-		{	
+		{
 			qDebug() << "Movement published";
-		
+
 			//delete message
 			if (!MyDbManager->DeleteMovement(movement.Id))
 			{
 				qDebug() << "Failed to delete movement!!";
-			}				
-		}	
+			}
+		}
 	}
 }
 
@@ -366,8 +367,8 @@ MainWindow::~MainWindow()
 {
 	digitalWrite(PINRED, LOW);
 	digitalWrite(PINGREEN, LOW);
-	digitalWrite(PINBLUE, LOW);	
-	
+	digitalWrite(PINBLUE, LOW);
+
 	delete MyApiClient;
 	delete MyNfcReader;
 	delete ui;
@@ -379,19 +380,19 @@ void MainWindow::ShowPersonDetailsDialog(const Dtos::Person & person)
 	PersonDetailsDialog* details = new PersonDetailsDialog(this);
 	details->setName(person.Name);
 	details->setMessage(person.InLocation ? QString("Welcome") : QString("Good Bye"));
-	
+
 	//image
 	if (!(person.Image.length() <= 0))
-	{		
+	{
 		QPixmap p;
-		p.loadFromData(person.Image);	
-		details->setImage(p);	
+		p.loadFromData(person.Image);
+		details->setImage(p);
 	}
-	
-	details->show();	
-	
-	QTimer::singleShot(2000, details, SLOT(hide())); 	
-	
+
+	details->show();
+
+	QTimer::singleShot(2000, details, SLOT(hide()));
+
 	//GREEN LED
 	digitalWrite(PINBLUE, HIGH);
 	digitalWrite(PINGREEN, LOW);
